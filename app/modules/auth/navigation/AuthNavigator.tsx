6a7c5c1acbbb399eff995624ac11bff26b870eb0
YU HENG GOH <22004880@siswa.um.edu.my>
2025-10-11 21:04:34 +0800

    Change dependency version

--- app/modules/auth/navigation/AuthNavigator.tsx
+++ app/modules/auth/navigation/AuthNavigator.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import SplashScreen from "../screens/SplashScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
